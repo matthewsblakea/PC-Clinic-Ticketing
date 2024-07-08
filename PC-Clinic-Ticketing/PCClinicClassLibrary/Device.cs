d301e169d6be72ffc651b1f18ea28e5ad23756a6
matthewsblakea <matthews.blake.a@gmail.com>
2024-07-08 17:22:56 -0400

    Setting up solution

---
+++ PC-Clinic-Ticketing/PCClinicClassLibrary/Device.cs
@@ -0,0 +1,12 @@
+﻿namespace PCClinicClassLibrary
+{
+    public class Device
+    {
+        public int DeviceID { get; set; }
+        public int UserID { get; set; }
+        public string DeviceType { get; set; }
+        public string ModelNumber { get; set; }
+        public string SerialNumber { get; set; }
+        public string DevicePassword { get; set; }
+    }
+}
