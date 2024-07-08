5f63764365ec822c8bba978aa1079a631570e467
matthewsblakea <matthews.blake.a@gmail.com>
2024-07-08 17:22:56 -0400

    Setting up solution

---
+++ PC-Clinic-Ticketing/PCClinicClassLibrary/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCClinicClassLibrary
+{
+    public class Ticket
+    {
+        public int TicketID { get; set; }
+        public int DeviceID { get; set; }
+        public string ReportedProblem { get; set; }
+        public string TechIntakeNotes { get; set; }
+        public string RepairStatus { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ PC-Clinic-Ticketing/PCClinicClassLibrary/User.cs
@@ -0,0 +1,12 @@
+﻿namespace PCClinicClassLibrary
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Phone1 { get; set; }
+        public string Phone2 { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
