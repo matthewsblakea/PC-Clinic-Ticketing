1e74b235cb4255ba1fe299d99afb78adf9d9f853
matthewsblakea <matthews.blake.a@gmail.com>
2024-08-03 14:29:52 -0400

    re-added files after cleanup

---
+++ PC-Clinic-Ticketing/PcClinicApi/Models/RepairLog.cs
@@ -0,0 +1,20 @@
+﻿namespace PcClinicApi.Models
+{
+    public class RepairLog
+    {
+            public enum logType
+            {
+                Repair = 0,
+                Contact = 1
+            }
+
+            public int RepairLogId { get; set; }
+            public int TicketId { get; set; }
+            public int UserId { get; set; }
+            public logType LogType { get; set; }
+            public DateTime LogTime { get; set; }
+            public string LogNotes { get; set; }
+            public User User { get; set; }
+            public Ticket Ticket { get; set; }
+        }
+}
---
+++ PC-Clinic-Ticketing/PcClinicApi/Models/User.cs
@@ -0,0 +1,25 @@
+﻿namespace PcClinicApi.Models
+{
+    public class User
+    {
+        public enum userType
+        {
+            Customer = 0,
+            Technician = 1,
+            Admin = 2
+        }
+
+        public int UserId { get; set; }
+        public string Password { get; set; }
+        public userType UserType { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone1 { get; set; }
+        public string? Phone2 { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public List<Device> Devices { get; set; }
+    }
+}
