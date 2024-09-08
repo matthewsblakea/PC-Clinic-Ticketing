5d499ea694dc436abd909147fa4405ed82dbe2bf
matthewsblakea <matthews.blake.a@gmail.com>
2024-09-08 17:07:35 -0400

    made UI prettier

--- PC-Clinic-Ticketing/PcClinicTicketingRazorUi/obj/Debug/net8.0/PcClinicTicketingRazorUi.AssemblyInfo.cs
+++ PC-Clinic-Ticketing/PcClinicTicketingRazorUi/obj/Debug/net8.0/PcClinicTicketingRazorUi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PcClinicTicketingRazorUi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc5207c5d267a4518ad14d5a7b1a8273d9ff4ec4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f85358588f8cdef82c219366a670099bd13756b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PcClinicTicketingRazorUi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PcClinicTicketingRazorUi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
